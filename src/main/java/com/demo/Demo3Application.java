71a2ecf786ec68760dadc9ef0cffcc3ca21a8ab3
zlb666 <2246097479@qq.com>
2018-04-04 17:00:33 +0800

    加载服务文件
    考虑到Mapper文件需要扫描，那么同样存在接口的服务层是否也需要扫描呢。

--- src/main/java/com/demo/Demo3Application.java
+++ src/main/java/com/demo/Demo3Application.java
@@ -4,9 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 @MapperScan("com.demo.dao")
+@Service("com.demo.service")
 public class Demo3Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
